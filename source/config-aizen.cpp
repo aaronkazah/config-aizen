0db51ef363eed1e5d5103bc1f04cfd3fd297323f
Aaron Kazah <aaronkazah@gmail.com>
2019-06-07 18:26:50 +0100

    Improved config-aizen.cpp by adding comments

--- source/config-aizen.cpp
+++ source/config-aizen.cpp
@@ -6,6 +6,7 @@
 
 
 void ConfigAizen::parseFile(const char *fileName) {
+    /*-- Parses config file and saves it to an unordered_map --*/
     std::ifstream configFile (fileName);
     if (configFile.is_open()){
         std::string line;
@@ -24,31 +25,35 @@ void ConfigAizen::parseFile(const char *fileName) {
             configMap.insert({key, value});
         }
     } else {
-        std::cerr << "Couldn't open config file";
+        std::cerr << "Couldn't open config file" << std::endl;
     }
 }
 
 std::unordered_map<std::string, std::string>::const_iterator ConfigAizen::findKey(std::string variableName) {
+    /*-- Tries to find key in our unordered_map --*/
     std::unordered_map<std::string, std::string>::const_iterator key = configMap.find(variableName);
     if (key == configMap.end()){
-        std::cerr << "Key does not exist";
+        std::cerr << "Key does not exist" << std::endl;
     }
     return key;
 }
 
 std::string ConfigAizen::getString(std::string variableName) {
+    /*-- Gets value from key and returns it as string  --*/
     auto key {findKey(variableName)};
     std::string value {key -> second };
     return value;
 };
 
 int ConfigAizen::getInt(std::string variableName) {
+    /*-- Gets value from key and returns it as an integer  --*/
     auto key {findKey(variableName)};
     int number {std::stoi(key -> second)};
     return number;
 };
 
 double ConfigAizen::getDouble(std::string variableName) {
+    /*-- Gets value from key and returns it as a double  --*/
     auto key {findKey(variableName)};
     double number {std::stof(key -> second)};
     return number;
@@ -56,6 +61,7 @@ double ConfigAizen::getDouble(std::string variableName) {
 };
 
 bool ConfigAizen::getBoolean(std::string variableName){
+    /*-- Gets value from key and returns it as a boolean  --*/
     auto key = findKey(variableName);
     std::string keyValue = key -> second;
     if (keyValue == "true") {
@@ -69,5 +75,6 @@ bool ConfigAizen::getBoolean(std::string variableName){
 };
 
 std::unordered_map<std::string, std::string> ConfigAizen::getConfigMap() {
+    /*-- Gives us access to our unordered_map  --*/
     return configMap;
 }
